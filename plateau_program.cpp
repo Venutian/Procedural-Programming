cae807e864e4dd54775c2c1dd33d6c14e7485a79
Your NameVenutian <yo3ls@myself.com>
2018-10-11 17:15:51 -0700

    plateau

--- plateau_program.cpp
+++ plateau_program.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+
 using namespace std;
+
+int maxlen(int a[], int n)
+{
+    int len = 1;
+    int fLen = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == a[i + 1])
+        {
+            len++;
+        }
+        else 
+        {if (fLen<len)
+            fLen = len;
+            len = 1;
+        }
+        
+            
+    }
+    return fLen;
+}
 
 int main()
 {
-    cout<<"Hello World";
-
+    int a[14] = {1, 2, 2, 2, 2, 3, 3, 3, 7, 7, 7, 7, 7, 7}, size = 14;
+    cout << maxlen(a, size) << endl;
     return 0;
 }
